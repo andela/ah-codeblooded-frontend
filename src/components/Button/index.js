f0b2e81e84ae5734c9d1696adb5980ddbda704d3
srmoffat <ngigemoffat@gmail.com>
2018-10-29 12:51:39 +0300

    chore(setup): setup empty react project
    - use create-react-app for bearbones
    - add eslint with airbnb configurations
    
    [Finishes #160577561]

---
+++ src/components/Button/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+  render() {
+    return (
+      <button>
+          Click Here
+      </button>
+    );
+  }
+}
+
+export default Button;
