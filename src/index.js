7039b273047098a6b0584bd7e78baf1a1017d7d6
= <=>
2018-10-29 19:35:59 +0300

    chore(redux setup): setup store and rootreducer
    -install react-redux, thunk, reduxImmuabilityStateInavariant
    -setup store
    -setup rootreducer
    -provide store to app entry point
    -fix tests
    
    [Maintains #160577562]
    
    chore(redux setup): add store
    
    -add store directory and file
    -update configStore function
    
    [Starts #160577562]
    
    chore(redux setup): add rootReducer
    
    -add rootReducer file
    -instantiate rootReducer
    
    [Starts #160577562]
    
    chore(redux setup): update store
    
    -rename config store file
    -make store available component
    
    [Starts #160577562]
    
    chore(redux setup): provide store
    
    -initialize store and provide store to components
    -connect component to store
    
    [Starts #160577562]
    
    chore(redux setup): update folder structure
    
    -move rootreduer to store
    -update store provider
    
    [Starts #160577562]
    
    chore(redux setup): fix linting issues
    
    -fix hound linting issues
    
    [Starts #160577562]
    
    chore(redux setup): fix linting issues
    
    -fix hound linting issues
    
    [Starts #160577562]
    
    chore(redux setup): fix linting issues
    
    -fix hound linting issue
    
    [Starts #160577562]
    
    chore(redux setup): fix linting issues
    
    -fix hound linting issues
    
    [Starts #160577562]
    
    chore(redux setup): fix linting issues
    
    -fix hound linting iisues
    
    [Starts  #160577562]
    
    chore(redux setup): fix linting issues
    
    -fix hound linting issues
    
    [Finishes #160577562]
    
    chore(redux setup): immutability state invariant
    
    -add immutability state invariant
    -remove yarn from project dependency
    
    [Starts #160577562]
    
    chore(redux setup): update eslint
    
    -add esversion to rules
    -update file extensions to rules
    
    [Starts #160577562]
    
    chore(redux setup): update test
    
    -fix failing travis test
    
    [Maintains #160577562]

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './assets/index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
+import configureStore from './store/index';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
+
 serviceWorker.unregister();
