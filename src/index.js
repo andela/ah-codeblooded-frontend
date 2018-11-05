5d9f9a75cef5daf11a6f2abe5d4f285bbc4bb5f5
Allan Imire <imireallan@gmail.com>
2018-11-05 16:05:40 +0300

    feat(like/dislike): implement environment variables
    - ensure all sensitive information is secured in the environment
    
    [Starts #160577579]

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import dotenv from 'dotenv';
 import 'materialize-css/dist/css/materialize.min.css';
 import './assets/css/index.scss';
 import Router from './routes';
 import * as serviceWorker from './serviceWorker';
 import configureStore from './store/index';
 import './env';
+
+
+dotenv.config();
 
 const store = configureStore();
 
