dd94b3edba0c0c68793ea78b87e5ea20fe302821
Allan Imire <imireallan@gmail.com>
2018-11-05 21:20:09 +0300

    feat(like/dislike): implement axios api module
    - ensure axios calls are modularized
    
    [Starts #160577579¬]

---
+++ src/utils/api.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+import config from './config';
+import getToken from './auth';
+
+
+const token = getToken();
+
+const uri = config.BASE_URL;
+
+const headers = {
+  'Content-Type': 'application/json',
+  Authorization: `Token ${token}`,
+};
+
+const api = {
+  get(endpoint) {
+    return axios({
+      method: 'get',
+      url: `${uri}${endpoint}`,
+      data: {},
+      config: {
+        headers,
+      },
+    });
+  },
+  post(endpoint, postData) {
+    return axios({
+      method: 'post',
+      url: `${uri}${endpoint}`,
+      data: JSON.stringify(postData),
+      config: {
+        headers,
+      },
+    });
+  },
+  put(endpoint, postData = null) {
+    return axios({
+      method: 'post',
+      url: `${uri}${endpoint}`,
+      data: JSON.stringify(postData),
+      config: {
+        headers,
+      },
+    });
+  },
+  delete(endpoint) {
+    return axios({
+      method: 'delete',
+      url: `${uri}${endpoint}`,
+      data: {},
+      config: {
+        headers,
+      },
+    });
+  },
+};
+
+
+export default api;
---
+++ src/utils/config.js
@@ -0,0 +1,5 @@
+const config = {
+  BASE_URL: process.env.API_URL,
+};
+
+export default config;
