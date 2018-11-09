f9424464871dcea77689b05ab38432534dca64aa
Yahya Hussein <husain.host@gmail.com>
2018-11-10 01:38:22 +0300

    feat(socialauth): Raise Test coverage for social-login
    - Add more test on action
    - Refactored the tests
    
    [Delivers #130635935]

--- src/pages/SocialLogin/state/action.test.js
+++ src/pages/SocialLogin/state/action.test.js
@@ -1,27 +1,72 @@
-import configureStore from "redux-mock-store";
 import * as actioncreator from "./SocialAction";
-import { SUCCESS_LOGIN, FAIL_LOGIN } from "./types";
+import socialLogin from "./SocialAction";
+import { SUCCESS_LOGIN, FAIL_LOGIN, START_LOGIN } from "./types";
+import moxios from "moxios";
+import thunk from "redux-thunk";
+import configureMockStore from "redux-mock-store";
 
-const mockstore = configureStore();
-const store = mockstore();
+const login = {
+  provider: "google",
+  access_token: "fake-token-here"
+};
+const get_action = [
+  { type: START_LOGIN },
+  { type: SUCCESS_LOGIN, payload: login }
+];
+const expectedActions = [
+  { type: START_LOGIN },
+  { type: SUCCESS_LOGIN, payload: login },
+  { type: FAIL_LOGIN, payload: login }
+];
+const urlGoogle =
+  "https://accounts.google.com/signin/&oauthgdpr=1&xsrfsig=AHgIfE-ybDu6ChemaBvNZ2iDWuQ3lP8jLw&flowName=GeneralOAuthFlow";
+const urlFacebook =
+  "https://www.facebook.com/v2.3/dialog/oauth?app_id=68148899890022";
+const middleWares = [thunk];
+const mockStore = configureMockStore(middleWares);
+const store = mockStore();
 
-describe("socialLogin", () => {
+describe("Social Login actions", () => {
   beforeEach(() => {
-    store.clearActions();
+    moxios.install();
+    jest.setTimeout = 300000;
   });
-  test("dispatches the correct action and payload ", () => {
-    const state = {
-      user: { email: "test@email.com", username: "jon doe", token: "token" }
-    };
-    const get_action = [{ type: SUCCESS_LOGIN, payload: state }];
-    store.dispatch(actioncreator.success(state));
+  afterEach(() => {
+    moxios.uninstall();
+  });
+  it("google-login should trigger the correct actions", () => {
+    moxios.stubRequest(urlGoogle, {
+      status: 200,
+      response: login
+    });
+    store
+      .dispatch(socialLogin("google", { token: "abcdefgh" }, []))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedAction);
+      });
+  });
+  it("facebook-login should trigger the correct actions", () => {
+    moxios.stubRequest(urlFacebook, {
+      status: 200,
+      response: login
+    });
+    const store = mockStore({
+      isAuthenticated: false
+    });
+    store
+      .dispatch(socialLogin("facebook", { token: "abcdefghe" }, []))
+      .then(() => {
+        expect(store.getActions()).toEqual(expectedAction);
+      });
+  });
+
+  it("dispatches the correct action and payload in action-creator ", () => {
+    store.dispatch(actioncreator.success(login));
     expect(store.getActions()).toEqual(get_action);
   });
 
-  test("dispatches the correct action and payload", () => {
-    const payload = "try again";
-    const expectedActions = [{ type: FAIL_LOGIN, payload }];
-    store.dispatch(actioncreator.fail(payload));
+  it("dispatches the correct action and payload in action-creator", () => {
+    store.dispatch(actioncreator.fail(login));
     expect(store.getActions()).toEqual(expectedActions);
   });
 });
