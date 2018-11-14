61ada3422d97867bacdbf1d075d499c8e1a0c818
= <=>
2018-11-14 13:36:57 +0300

    feat(favorite articles): enable users to favourite articles
    - add favorite and unfavorite actions
    - add favorite and unfavorite reducers
    - add favorite article renderer
    - test actions reducers and renderer
    - add view favourites functionality
    
    [Starts #160577588]
    
    feat(favorite articles): add favorite and unfavorite functionality
    
    - add favorite and unfavorite actions
    - add favorite and unfavorite reducers
    - add favorite article renderer
    
    [Starts #160577588]
    
    feat(favorite articles): add favorite and unfavorite functionality
    
    - add favorite and unfavorite actions
    - add favorite and unfavorite reducers
    - add favorite article renderer
    
    [Starts #160577588]
    
    feat(favorite articles): add favorite and unfavorite functionality
    
    - add favorite and unfavorite actions
    - add favorite and unfavorite reducers
    - add favorite article renderer
    
    [Starts #160577588]
    
    feat(favorite articles): implement favourite unfavourite toggle
    
    - implement conditional rendering
    
    [Starts #160577588]
    
    feat(favorite articles): update tests on actions
    
    - add test favourite action
    - add test unfavourite action
    - add test errors
    
    [Starts #160577588]
    
    feat(favorite articles): fix hound issues
    
    - fix hound issues
    
    [Starts #160577588]
    
    feat(favorite articles): add favourite unfavourite functionality
    
    - update favourite unfavourite toggle
    
    [Starts #160577588]
    
    Update .env

--- src/containers/profiles/ViewProfiles/index.js
+++ src/containers/profiles/ViewProfiles/index.js
@@ -6,6 +6,8 @@ import PropTypes from 'prop-types';
 import avatar2 from '../../../assets/images/avatar2.png';
 import { getUserProfileAction } from '../state/actions';
 import EditProfiles from '../EditProfiles';
+import ConnectedArticleListing from '../../ArticleListing';
+import layouts from '../../../components/ArticleCard/layouts';
 import './ViewProfiles.scss';
 import NavBar from '../../NavBar';
 
@@ -15,6 +17,7 @@ class ViewProfiles extends Component {
   componentDidMount = () => {
     const { history, user } = this.props;
     const modals = document.querySelector('.modal');
+    Materialize.Tabs.init(document.querySelector(".tabs"), {});
     Materialize.Modal.init(modals, {});
     const { getUserProfile } = this.props;
     getUserProfile(user.username);
@@ -24,26 +27,62 @@ class ViewProfiles extends Component {
   render() {
     const { profile } = this.props;
     return (
-      <div>
-        <NavBar {...this.props} />
-        <EditProfiles {...this.props} />
-        <div className="container">
-          <div className="card" styles="margin-top:40px">
-            <div className="card-content">
-              <a className="small modal-trigger right" href="#edit-profile-modal"><i className="material-icons">create</i></a>
-              <div className="row">
-                <div className="col"><span className="flow-text"><img src={profile.image || avatar2} className="responsive-img circle profile" alt="profile" /></span></div>
-                <div className="col s8">
-                  <span className="flow-text">
-                    <h5 styles="font-weight: bolder">{profile && profile.username}</h5>
-                  </span>
-                  <p>{ profile && profile.bio }</p>
+      <>
+        <div>
+          <NavBar {...this.props} />
+          <EditProfiles {...this.props} />
+          <div className="container">
+            <div className="card" styles="margin-top:40px">
+              <div className="card-content">
+                <a className="small modal-trigger right" href="#edit-profile-modal"><i className="material-icons">create</i></a>
+                <div className="row">
+                  <div className="col"><span className="flow-text"><img src={profile.image || avatar2} className="responsive-img circle profile" alt="profile" /></span></div>
+                  <div className="col s8">
+                    <span className="flow-text">
+                      <h5 styles="font-weight: bolder">{profile && profile.username}</h5>
+                    </span>
+                    <p>{ profile && profile.bio }</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+        <div className="row">
+          <div className="col s12 m8 offset-m2">
+            <div>
+              <ul className="tabs">
+                <li className="tab col s3">
+                  <a className="active" href="#favourites">
+                    Favourite Articles
+                  </a>
+                </li>
+                <li className="tab col s3">
+                  <a href="#followers">
+                    Followers
+                  </a>
+                </li>
+                <li className="tab col s3">
+                  <a href="#following">
+                    Following
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div id="favourites">
+              <ConnectedArticleListing
+                {...this.props}
+                layoutProvider={() => layouts.MINIMAL_AUTHOR_LAYOUT}
+                emptyMessage="You do not have any favourites"
+                url="user/articles/favourites/"
+                listName="favourites"
+              />
+            </div>
+            <div id="followers" />
+            <div id="following" />
+          </div>
+        </div>
+        </>
     );
   }
 }
